bf742ef910d534b1cb711bcc303b851eadb46a23
Sadik <sadik7070@gmail.com>
2024-12-29 16:07:47 +0400

    city validation fixed

--- app/Http/Requests/City/StoreCityRequest.php
+++ app/Http/Requests/City/StoreCityRequest.php
@@ -3,26 +3,26 @@
 namespace App\Http\Requests\City;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCityRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => [
+                'required',
+                'max:200',
+                Rule::unique('cities')->whereNull('deleted_at'),
+            ],
+            'short_code' => [
+                'nullable',
+                Rule::unique('cities')->where('deleted_at'),
+            ],
+            'country_id' => 'required|integer',
+            'state_id' => 'required|integer',
+            'status' => 'required|boolean',
         ];
     }
 }
--- app/Http/Requests/City/UpdateCityRequest.php
+++ app/Http/Requests/City/UpdateCityRequest.php
@@ -3,26 +3,25 @@
 namespace App\Http\Requests\City;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateCityRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => [
+                'required',
+                'max:200',
+                Rule::unique('cities')->whereNull('deleted_at')->ignore($this->city),
+            ],
+            'short_code' => [
+                'nullable',
+                Rule::unique('cities')->where('deleted_at')->ignore($this->city),
+            ],
+            'country_id' => 'required|integer',
+            'state_id' => 'required|integer',
+            'status' => 'required|boolean',
         ];
     }
 }
