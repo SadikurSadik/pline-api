42c6a50cd5041bf06724fa9c5cf53470fd82c16e
Sadik <sadik7070@gmail.com>
2024-12-31 09:27:38 +0400

    customer detail api fixed

--- app/Http/Resources/Customer/CustomerDetailResource.php
+++ app/Http/Resources/Customer/CustomerDetailResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources\Customer;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 
 class CustomerDetailResource extends JsonResource
 {
@@ -20,10 +22,39 @@ class CustomerDetailResource extends JsonResource
             'name' => $this->name,
             'company_name' => $this->company_name,
             'username' => $this->user?->username,
+            'profile_photo' => $this->getProfilePhoto($this->user?->profile_photo),
             'email' => $this->user?->email,
             'phone' => $this->phone,
+            'trn' => $this->trn,
+            'address' => $this->address,
+            'buyer_ids' => $this->buyer_ids,
+            'country_id' => $this->country_id,
+            'country_name' => $this->country?->name,
+            'state_id' => $this->state_id,
+            'state_name' => $this->state?->name,
+            'city_id' => $this->city_id,
+            'city_name' => $this->city?->name,
+            'category' => $this->category,
+            'documents' => $this->customerDocuments($this->documents ?? []),
             'status' => $this->user?->status,
             'status_name' => ! empty($this->user) ? $this->user->status->getLabel() : '',
         ];
+    }
+
+    private function customerDocuments($documents)
+    {
+        return Arr::map($documents, function ($document) {
+            return filter_var($document, FILTER_VALIDATE_URL) === false ? Storage::url($document) : $document;
+        });
+    }
+
+    private function getProfilePhoto(mixed $profilePhoto)
+    {
+        if (empty($profilePhoto)) {
+            // return default profile photo
+            return '';
+        }
+
+        return filter_var($profilePhoto, FILTER_VALIDATE_URL) ? $profilePhoto : Storage::url($profilePhoto);
     }
 }
--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -56,11 +56,19 @@ class CustomerService
         }
         $user->fill($data);
         $user->role_id = Role::CUSTOMER->value;
+        if(! empty($user->profile_photo)) {
+            $user->profile_photo = getRelativeUrl($user->profile_photo);
+        }
         $user->save();
         $user->syncRolePermissions();
 
         $customer->fill($data);
         $customer->user_id = $user->id;
+        if(! empty($customer->documents)) {
+            $customer->documents = Arr::map($customer->documents, function ($document) {
+                return getRelativeUrl($document);
+            });
+        }
         $customer->save();
 
         return $customer;
