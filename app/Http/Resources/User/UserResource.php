d0fb33120cd089391b3d3affdfec635e0bc66d56
Sadik <sadik7070@gmail.com>
2025-01-08 09:57:44 +0400

    accounting login url added

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources\User;
 
+use App\Enums\Role;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
@@ -18,6 +20,7 @@ class UserResource extends JsonResource
             'role_name' => $this->role_id->name,
             'status_name' => $this->status->getLabel(),
             'profile_photo' => $this->getProfilePhoto($this->profile_photo),
+            'accounting_login_url' => $this->getAccountingLoginUrlProperty(),
         ];
     }
 
@@ -28,5 +31,23 @@ class UserResource extends JsonResource
         }
 
         return Storage::url($photo);
+    }
+
+    private function getAccountingLoginUrlProperty(): ?string
+    {
+        if( !in_array( auth()->user()->role_id, [Role::OWNER, Role::SUPER_ADMIN, Role::ACCOUNTANT] ) ) {
+            return null;
+        }
+
+        $userId = auth()->user()->id;
+        $encUserId = Crypt::encryptString($userId);
+
+        $data = [
+            'userId' => $encUserId
+        ];
+
+        $encData = urlencode( json_encode($data) );
+
+        return env('ACCOUNTING_APP_URL') . '/accounting-auth?enc_data='. $encData;
     }
 }
