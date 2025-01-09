b85e23c9069adcee42922fd47f6c1b221a31a6a2
Sadik <sadik7070@gmail.com>
2025-01-09 08:21:06 +0400

    issue fixed

--- app/Filters/FilterByArrivedDateOnVehicleContainerRelation.php
+++ app/Filters/FilterByArrivedDateOnVehicleContainerRelation.php
@@ -9,14 +9,16 @@ class FilterByArrivedDateOnVehicleContainerRelation extends BaseFilter
 {
     public function handle($query, Closure $next, $data)
     {
-        $query->whereHas('vehicle.container', function ($query) use ($data) {
-            if (Str::contains($data['arrival_date'] ?? '', ' to ')) {
-                $range = dateRangeToDateTimeRange(explode(' to ', $data['arrival_date']));
-                $this->whereDateBetweenFilter($query, 'arrival_date', $range);
-            } elseif (! empty($data['arrival_date'])) {
-                $this->whereFilter($query, 'arrival_date', $data['arrival_date']);
-            }
-        });
+        if(!empty($data['arrival_date'])) {
+            $query->whereHas('vehicle.container', function ($query) use ($data) {
+                if (Str::contains($data['arrival_date'] ?? '', ' to ')) {
+                    $range = dateRangeToDateTimeRange(explode(' to ', $data['arrival_date']));
+                    $this->whereDateBetweenFilter($query, 'arrival_date', $range);
+                } elseif (! empty($data['arrival_date'])) {
+                    $this->whereFilter($query, 'arrival_date', $data['arrival_date']);
+                }
+            });
+        }
 
         return $next($query);
     }
