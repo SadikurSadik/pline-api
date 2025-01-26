b885b1a8d1e31bf435c1c601a9a7f5b03e09a8c3
Sadik <sadik7070@gmail.com>
2025-01-26 17:14:55 +0400

    issue fixed

--- app/Services/RoleService.php
+++ app/Services/RoleService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Filters\FilterByExceptIds;
 use App\Filters\FilterByName;
+use App\Models\User;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\Permission\Models\Role;
@@ -32,6 +33,13 @@ class RoleService
         $role->save();
 
         $role->syncPermissions($data['permissions']);
+
+        if($id == 3) {
+            // customer role
+            foreach (User::where('role_id', $id)->cursor() as $customer) {
+                $customer->syncPermissions($data['permissions']);
+            }
+        }
 
         return $role;
     }
